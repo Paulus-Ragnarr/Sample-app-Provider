93dc80b33b9ffdfe36d0baee0d4e02223d0d806b
Paul Jairus Caberte <102200899+Paulus-Ragnarr@users.noreply.github.com>
2023-08-03 14:11:17 +0800

    Setting up main, my_app, and home_view

---
+++ lib/ui/views/app/my_app.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:my_app_provider/ui/views/homepage/home_view.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const HomeView(),
+    );
+  }
+}
---
+++ lib/ui/views/homepage/home_view.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class HomeView extends StatelessWidget {
+  const HomeView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 25.0),
+        child: Column(
+          children: [
+            AppBar(
+              
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
